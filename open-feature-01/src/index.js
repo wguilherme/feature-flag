33f7b5ad3bddcc481844c46da05bd56de8bb9b2c
wguilherme <wguilhermecriativo@gmail.com>
2025-01-17 15:51:06 -0300

    feat: adiciona flagd no k8s, automação para port-forward, flags.yaml para declaração e estado das ff

--- open-feature-01/src/index.js
+++ open-feature-01/src/index.js
@@ -1,11 +1,29 @@
+// app.js
 const express = require('express');
+const { OpenFeature } = require('@openfeature/js-sdk');
+const { FlagdProvider } = require('@openfeature/flagd-provider');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World2' });
+// Configurar OpenFeature
+const provider = new FlagdProvider({
+  host: 'flagd', // nome do service no K8s
+  port: 8013
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+OpenFeature.setProvider(provider);
+const client = OpenFeature.getClient();
+
+app.get('/', async (req, res) => {
+  const novaFuncaoAtiva = await client.getBooleanValue('nova-funcao', false);
+
+  console.log('novaFuncaoAtiva', novaFuncaoAtiva);
+  
+  if (novaFuncaoAtiva) {
+    res.send('Nova função ativa!');
+  } else {
+    res.send('Função desativada');
+  }
 });
+
+app.listen(3000);
