cd7292b09a78d1d7df07f7a41b81c229997bb2c1
wguilherme <wguilhermecriativo@gmail.com>
2025-01-17 15:29:19 -0300

    feat: adiciona automacao para deploy no kind local

--- open-feature-01/src/index.js
+++ open-feature-01/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World2' });
 });
 
 app.listen(port, () => {
